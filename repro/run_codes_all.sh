fd99553ea3efe1ecbef5698877546879d596482c
araeeeeee <shota.arai.c2@tohoku.ac.jp>
2025-12-25 08:36:48 +0900

    Update run_codes_all.sh

--- repro/run_codes_all.sh
+++ repro/run_codes_all.sh
@@ -10,6 +10,9 @@ cd section3_1_2 && bash run_codes.sh && cd ..
 echo "Running Section 3.1.3 ..."
 cd section3_1_3 && bash run_codes.sh && cd ..
 
+echo "Running Section 3.1.4 ..."
+cd section3_1_4 && bash run_codes.sh && cd ..
+
 echo "Running Section 3.2 ..."
 cd section3_2 && bash run_codes.sh && cd ..
 
